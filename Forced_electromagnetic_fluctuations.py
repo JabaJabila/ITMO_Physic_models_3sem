593bd1a07fa69cff4d1762c665efb061b743a005
quvi <quvivirus@vk.com>
2022-01-11 00:11:17 +0300

    feat: added normal graphs and excel tables

--- Forced_electromagnetic_fluctuations.py
+++ Forced_electromagnetic_fluctuations.py
@@ -16,24 +16,24 @@ f_rez = (sqrt(1 / (C * L) - R ** 2 / (2 * L ** 2))) / (2 * pi)
 print('Resonant frequency:', f_rez)
 
 workbook = xlsxwriter.Workbook('model.xlsx')
-worksheet = workbook.add_worksheet()
-worksheet.write('A1', 'f, Hz')
-worksheet.write('B1', 'X, Om')
-worksheet.write('C1', 'I0, A')
-worksheet.write('D1', 'U, V')
+U_f_sheet = workbook.add_worksheet("Dependence U from f")
+U_f_sheet.write('A1', 'f, Hz')
+U_f_sheet.write('B1', 'X, Om')
+U_f_sheet.write('C1', 'I0, A')
+U_f_sheet.write('D1', 'U, V')
 col = 0
 
 # array of frequencies
 f_arr = np.arange(f_rez - 500, f_rez + 500, 50)
 for row in range(len(f_arr)):
-    worksheet.write(row + 1, col, f_arr[row])
+    U_f_sheet.write(row + 1, col, f_arr[row])
 
 col += 1
 # array of impedance
 X_arr = []
 for i in range(len(f_arr)):
     X_arr.append(sqrt(R ** 2 + (f_arr[i] * 2 * pi * L - 1 / (f_arr[i] * 2 * pi * C)) ** 2))
-    worksheet.write(i + 1, col, X_arr[i])
+    U_f_sheet.write(i + 1, col, X_arr[i])
 
 col += 1
 # array of current amplitude
@@ -42,9 +42,9 @@ I0_arr = []
 U_arr = []
 for i in range(len(f_arr)):
     I0_arr.append(E / X_arr[i])
-    worksheet.write(i + 1, col, I0_arr[i])
+    U_f_sheet.write(i + 1, col, I0_arr[i])
     U_arr.append(I0_arr[i] / (f_arr[i] * 2 * pi * C))
-    worksheet.write(i + 1, col + 1, U_arr[i])
+    U_f_sheet.write(i + 1, col + 1, U_arr[i])
 
 fig, axs = plt.subplots(2, 1, constrained_layout=True)
 axs[0].plot(f_arr, U_arr, '-')
@@ -78,14 +78,22 @@ fnew = func(cnew)
 axs[1].plot(C_arr, f_rez_arr, 'o', cnew, fnew, '-')
 axs[1].set_title('Dependence of the square of the resonant frequency\n on the reverse capacitance')
 axs[1].set_xlabel('C^-1, 1/F')
-axs[1].set_ylabel('f rez^2, Hz')
+axs[1].set_ylabel('f rez^2, Hz^2')
 
 k = (fnew[0] - fnew[1]) / (cnew[0] - cnew[1])
 b = fnew[1] - k * cnew[1]
 
 R_graph = sqrt(abs(b) * 4 * k)
 
-print(k ** -1, b, R_graph)
+print('Сoil inductance according to the graph:', k)
+print('Active resistance according to the graph:', R_graph)
+
+f_C_sheet = workbook.add_worksheet("Dependence f from C")
+f_C_sheet.write('A1', 'C^-1, 1/F')
+f_C_sheet.write('B1', 'f rez^2, Hz^2')
+for i in range(len(C_arr)):
+    f_C_sheet.write(i+1, 0, C_arr[i])
+    f_C_sheet.write(i+1, 1, f_rez_arr[i])
 
 plt.show()
 workbook.close()
